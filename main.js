2ff23f904ab77ec1b79e3423a249ff589ea208f7
BenoitCochet <44063006+BenoitCochet@users.noreply.github.com>
2018-10-25 16:56:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,30 +3,54 @@ var app = new Vue({
   data: {
     message: 'Hello Vue!',
     users: [],
-    userSelected: '',
-    info: 'Mokui',
-    
+    userSelected: [],
+    infos: [],
+    datedeb: '',
+    datefin: '',
   },
   created: function() 
   {
     vm = this;
-    $.getJSON( "data.json", function( json ) {
-      vm.users = json;
-      console.log("my users: " + JSON.stringify(vm.users));
-      
-    })
+   // $.getJSON( "data.json", function( json ) {
+    vm.users = JSON.parse(getNom());
+    console.log(vm.users);
+    console.log("my users: " + JSON.stringify(JSON.parse(vm.users)));
+   // })
   },
-  mounted: {
-
+  mounted: function() {
+    var self = this;
+    $('#datepicker').datepicker({
+      onSelect:function(selectedDate, datePicker) {            
+        self.datedeb = selectedDate;
+      }
+    });
+    $('#datepicker1').datepicker({
+      onSelect:function(selectedDate1, datePicker1) {            
+        self.datefin = selectedDate1;
+      }
+    });
   },
   methods: {
-    getSelectedUser : function(){
-      vm = this;
+    getSelectedUsers : function(){
+      this.infos = [];
+      var date = ""
+      if(this.datedeb!="")
+        date += "&since=" + this.datedeb;
+      if(this.datefin!="")
+        date += "&until=" + this.datefin;
       console.log("User: "+vm.userSelected);
-      
-      axios
-        .get('https://api.github.com/users/'+vm.userSelected)
-        .then(response => (vm.info = response))
+      for(var user in vm.userSelected){
+        axios
+          .get('https://api.github.com/repos/'+vm.userSelected[user]+'/github-ynov-vue/commits?'+date,
+            {headers: {
+              Authorization: "Bearer 691964033f40e80ef152704ab688cde8615f994a"
+            }})
+          .then(function(response){
+            this.vm.infos = this.vm.infos.concat(response.data);
+          })
+
+        var date = this.datedeb;
+      }
     }
   },
   computed: {
